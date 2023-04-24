40a7324336106777cef8c44440b02a6eb62f2de6
Michael Coca-Vargas <68833274+Michaelcoav@users.noreply.github.com>
2023-04-23 23:26:18 -0400

    Changed class name to flex container.

--- src/Components/question.js
+++ src/Components/question.js
@@ -1,6 +1,6 @@
 const Question = ({prompt, answerChoices}) => {
     return (
-        <div className="question">
+        <div className="flex-container">
             <div>
                 {prompt}
                 <br />
