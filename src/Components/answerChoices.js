9d951a5069150bee744fbda631da57fc549c7fc8
Michael Coca-Vargas <68833274+Michaelcoav@users.noreply.github.com>
2023-04-23 23:23:44 -0400

    Removed stack, used nanoid to create unique id, as well as used ordered list and list item to store all the answer choices.

--- src/Components/answerChoices.js
+++ src/Components/answerChoices.js
@@ -1,16 +1,22 @@
-import { Stack } from "@mui/material";
 import Choice from "./choice";
+import { nanoid } from "nanoid";
 
+// represents all choices possible for a single question
 const AnswerChoices = ({choices}) => {
     return (
-        <Stack spacing={2}>
-            {choices.map((choice, index) => {
+        <ol className="list">
+            {choices.map((choice) => {
                 const answerChoice = choice.answerChoice;
                 const isCorrect = choice.isCorrect;
 
-                return<Choice key={index} answerChoice={answerChoice} isCorrect={isCorrect}/>;
+                return (
+                    <li key={nanoid()}>
+                        <Choice answerChoice={answerChoice} isCorrect={isCorrect}/>
+                    </li>
+                );
+                
             })}
-        </Stack>
+        </ol>
     );
 };
 
