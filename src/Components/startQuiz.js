8893e3ddffba574728ca5aa44b7152ba492838fc
Michael Coca-Vargas <68833274+Michaelcoav@users.noreply.github.com>
2023-04-23 23:28:06 -0400

    Created to show a card with the start button to start quiz.

--- src/Components/startQuiz.js
+++ src/Components/startQuiz.js
@@ -1,5 +1,20 @@
-const StartQuiz = () => {
+import { Card, CardContent } from "@mui/material";
 
+// representing button that will render the quiz once it is clicked
+const StartQuiz = ({hasStarted, setHasStarted}) => {
+    const startQuiz = () => {
+        setHasStarted(!hasStarted);
+    };
+
+    return (
+        <Card className="start-quiz">
+            <CardContent className="flex-container" variant="outlined">
+                <button onClick={startQuiz}>
+                    Start
+                </button>
+            </CardContent>
+        </Card>
+    )
 };
 
 export default StartQuiz;
